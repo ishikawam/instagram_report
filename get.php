eb4251eac2906310c113277eed10c1df1d98c0c3
M_Ishikawa <ishikawam@nifty.com>
2018-03-02 20:48:38 +0900

    メモリを節約

--- get.php
+++ get.php
@@ -6,60 +6,74 @@
  *
  */
 
-// ユーザー一覧を取得
-$users = getAccounts();
+run();
 
-// アカウント名が有効かどうかのチェック
-$data = validateAccounts($users);
+function run() {
+    $configFile = __DIR__ . '/config/config.php';
+    require($configFile); // $config
 
-// アクセスするURLを絞る
-$urls = array_unique(array_column($data, 'url'));
+    // ユーザー一覧を取得してアカウント名が有効かどうかのチェック
+    $datas = validateAccounts(getAccounts());
 
-// Instagram取得
-$result = getContents($urls);
-echo "\n";
+    $fp = fopen(__DIR__ . '/out/report.csv', 'w');
+    $isFirst = true;
 
-// 200, 404以外をリトライ
-$urls = [];
-foreach ($result as $val) {
-    if (! in_array($val['status'], [200, 404])) {
-        $urls[] = $val['url'];
+    $count = 0;
+    $all = count($datas);
+
+    foreach (array_chunk($datas, $config['thread']) as $data) {
+        echo(sprintf(" %s/%s\n", $count * $config['thread'], $all));
+        $count ++;
+
+        // アクセスするURLを絞る @todo; $datasの段階で絞らないと意味ない。
+        $urls = array_unique(array_column($data, 'url'));
+
+        // Instagram取得
+        $result = getContents($urls, $config['timeout']);
+        sleep($config['wait']);
+
+        // 200, 404以外をリトライ
+        $urls = [];
+        foreach ($result as $val) {
+            if (! in_array($val['status'], [200, 404])) {
+                $urls[] = $val['url'];
+            }
+        }
+        if ($urls) {
+            echo "Retry.\n";
+            $resultRetry = getContents($urls);
+            $result = array_merge($result, $resultRetry);
+            echo "\n";
+        }
+
+        // parse
+        $data = parse($data, $result);
+
+        // パースエラーはリトライ
+        $urls = [];
+        foreach ($data as $val) {
+            if ($val['error'] == 'Parse error') {
+                $urls[] = $val['url'];
+            }
+        }
+
+        if ($urls) {
+            echo "Retry for Parse error.\n";
+            $resultRetry = getContents($urls);
+            $result = array_merge($result, $resultRetry);
+            $data = parse($data, $result);
+            echo "\n";
+        }
+
+        // レポート
+        putReport($fp, $data, $isFirst);
+        $isFirst = false;
     }
+
+    fclose($fp);
+
+    echo("\nFinished.\n\n");
 }
-if ($urls) {
-    echo "Retry.\n";
-    $resultRetry = getContents($urls);
-    $result = array_merge($result, $resultRetry);
-    echo "\n";
-}
-
-// parse
-$data = parse($data, $result);
-echo "\n";
-
-// パースエラーはリトライ
-$urls = [];
-foreach ($data as &$val) {
-    if ($val['error'] == 'Parse error') {
-        $urls[] = $val['url'];
-    }
-}
-if ($urls) {
-    echo "Retry for Parse error.\n";
-    $resultRetry = getContents($urls);
-    $result = array_merge($result, $resultRetry);
-    $data = parse($data, $result);
-    echo "\n";
-}
-
-// レポート配信
-putReport($data);
-
-echo("\nFinished.\n\n");
-
-
-///////////////////////////////////////////////////////////////////////////
-
 
 /**
  * ユーザー一覧を取得
@@ -108,89 +122,78 @@ function validateAccounts($users)
 /**
  * Instagram取得
  * @param string[] $urls
+ * @param int $timeout
  * @return array [][content, status, url]
  */
-function getContents($urls)
+function getContents($urls, $timeout)
 {
-    $configFile = __DIR__ . '/config/config.php';
-    require($configFile); // $config
-
     $mh = curl_multi_init();
     $result = [];
 
-    $count = 0;
-    $all = count($urls);
-    while ($urlArray = array_splice($urls, 0, $config['thread'])) {
-        echo(sprintf(" %s/%s\n", $count * $config['thread'], $all));
-        $count ++;
-
-        foreach ($urlArray as $url) {
-            if (! $url) {
-                continue;
-            }
-            $ch = curl_init();
-            curl_setopt_array($ch, [
-                    CURLOPT_URL             => $url,
-                    CURLOPT_RETURNTRANSFER  => true,
-                    CURLOPT_TIMEOUT         => $config['timeout'],
-                    CURLOPT_CONNECTTIMEOUT  => $config['timeout'],
-                    CURLOPT_FOLLOWLOCATION  => true, // リダイレクトを追いかける
-                    CURLOPT_MAXREDIRS       => 10, // リダイレクト回数上限
-                    CURLOPT_AUTOREFERER     => true, // リダイレクトの際にヘッダのRefererを自動的に追加させる
-                ]);
-            curl_multi_add_handle($mh, $ch);
+    foreach ($urls as $url) {
+        if (! $url) {
+            continue;
         }
-
-        do {
-            $stat = curl_multi_exec($mh, $running);
-        } while ($stat === CURLM_CALL_MULTI_PERFORM);
-        if (! $running || $stat !== CURLM_OK) {
-            throw new RuntimeException('おかしなURLが混ざっているかも');
-        }
-
-        do {
-            switch (curl_multi_select($mh, $config['timeout'])) {
-                // イベントが発生するまでブロック
-
-                case -1:
-                    sleep(1); // ちょっと待ってからretry
-                    echo "[sleep]\n";
-                    do {
-                        $stat = curl_multi_exec($mh, $running);
-                    } while ($stat === CURLM_CALL_MULTI_PERFORM);
-                    continue 2;
-
-                case 0: // タイムアウト
-                    echo "[timeout]\n";
-                    continue 2; // retry
-
-                default:
-                    do {
-                        $stat = curl_multi_exec($mh, $running); // ステータスを更新
-                    } while ($stat === CURLM_CALL_MULTI_PERFORM);
-
-                    do {
-                        if ($raised = curl_multi_info_read($mh, $remains)) {
-                            // 変化のあったcurlハンドラを取得する
-                            $info = curl_getinfo($raised['handle']);
-                            if ($info['http_code'] != 200) {
-                                echo "$info[http_code] : $info[url]\n";
-                            }
-
-                            $result[$info['url']] = [
-                                'content' => curl_multi_getcontent($raised['handle']), // エラー時はfalse
-                                'status' => $info['http_code'], // 正常時は200
-                                'url' => $info['url'],
-                            ];
-                            curl_multi_remove_handle($mh, $raised['handle']);
-                            curl_close($raised['handle']);
-                        }
-                    } while ($remains);
-            }
-        } while ($running);
-
-        sleep($config['wait']);
+        $ch = curl_init();
+        curl_setopt_array($ch, [
+                CURLOPT_URL             => $url,
+                CURLOPT_RETURNTRANSFER  => true,
+                CURLOPT_TIMEOUT         => $timeout,
+                CURLOPT_CONNECTTIMEOUT  => $timeout,
+                CURLOPT_FOLLOWLOCATION  => true, // リダイレクトを追いかける
+                CURLOPT_MAXREDIRS       => 10, // リダイレクト回数上限
+                CURLOPT_AUTOREFERER     => true, // リダイレクトの際にヘッダのRefererを自動的に追加させる
+            ]);
+        curl_multi_add_handle($mh, $ch);
     }
+
+    do {
+        $stat = curl_multi_exec($mh, $running);
+    } while ($stat === CURLM_CALL_MULTI_PERFORM);
+    if (! $running || $stat !== CURLM_OK) {
+        throw new RuntimeException('おかしなURLが混ざっているかも');
+    }
+
+    do {
+        switch (curl_multi_select($mh, $timeout)) {
+            // イベントが発生するまでブロック
+
+            case -1:
+                sleep(1); // ちょっと待ってからretry
+                echo "[sleep]\n";
+                do {
+                    $stat = curl_multi_exec($mh, $running);
+                } while ($stat === CURLM_CALL_MULTI_PERFORM);
+                continue 2;
+
+            case 0: // タイムアウト
+                echo "[timeout]\n";
+                continue 2; // retry
+
+            default:
+                do {
+                    $stat = curl_multi_exec($mh, $running); // ステータスを更新
+                } while ($stat === CURLM_CALL_MULTI_PERFORM);
+
+                do {
+                    if ($raised = curl_multi_info_read($mh, $remains)) {
+                        // 変化のあったcurlハンドラを取得する
+                        $info = curl_getinfo($raised['handle']);
+                        if ($info['http_code'] != 200) {
+                            echo "$info[http_code] : $info[url]\n";
+                        }
+
+                        $result[$info['url']] = [
+                            'content' => curl_multi_getcontent($raised['handle']), // エラー時はfalse
+                            'status' => $info['http_code'], // 正常時は200
+                            'url' => $info['url'],
+                        ];
+                        curl_multi_remove_handle($mh, $raised['handle']);
+                        curl_close($raised['handle']);
+                    }
+                } while ($remains);
+        }
+    } while ($running);
 
     curl_multi_close($mh);
 
@@ -228,25 +231,26 @@ function parse($data, $result)
             continue;
         }
 
-        $json = json_decode($out[1]);
+        $json = json_decode($out[1], true); // @todo; メモリ節約のため配列に変更したい
         if ($json === null) {
             $val['error'] = 'Json error';
             echo(sprintf("ERROR: Json error '%s'\n", $user));
             file_put_contents(__DIR__ . '/out/error/ERROR_JSON_' . $user, $out[1]);
-        } elseif (! isset($json->entry_data->ProfilePage[0]->user)) {
+        } elseif (! isset($json['entry_data']['ProfilePage'][0]['user'])) {
             // ユーザーが存在しない
             $val['error'] = 'No user error';
             echo(sprintf("ERROR: No user error '%s'\n", $user));
-        } elseif ($json->entry_data->ProfilePage[0]->user->is_private) {
+        } elseif ($json['entry_data']['ProfilePage'][0]['user']['is_private']) {
             // 鍵付きユーザー
             $val['error'] = 'Private user';
             echo(sprintf("ERROR: Private user '%s'\n", $user));
         }
 
-        $val['data'] = $json;
+        $val['data'] = $json['entry_data']['ProfilePage'][0]['user'] ?? null;
 
         file_put_contents(__DIR__ . '/out/html/' . $user, $out[1]);
     }
+
     return $data;
 }
 
@@ -254,12 +258,10 @@ function parse($data, $result)
  * レポート配信
  * @param array $data [][data, user, error]
  */
-function putReport($data)
+function putReport($fp, $data, $isFirst = false)
 {
-    $fp = fopen(__DIR__ . '/out/report.csv', 'w');
-    $isFirst = true;
     foreach ($data as $row) {
-        if (! isset($row['data']->entry_data->ProfilePage[0]->user)) {
+        if (($row['data'] ?? null) === null) {
             fputcsv($fp, [$row['user'], $row['error']]);
             if ($isFirst) {
                 // 1行目からエラーだったら強制終了
@@ -268,21 +270,21 @@ function putReport($data)
             }
             continue;
         }
-        $userData = $row['data']->entry_data->ProfilePage[0]->user;
+        $userData = $row['data'];
 
         $csv = [
             'user' => $row['user'],
             'status' => $row['error'],
-            'follows' => $userData->follows->count,
-            'followers' => $userData->followed_by->count,
-            'articles' => $userData->media->count,
+            'follows' => $userData['follows']['count'],
+            'followers' => $userData['followed_by']['count'],
+            'articles' => $userData['media']['count'],
         ];
 
-        foreach ($userData->media->nodes as $key => $article) {
-            $csv[$key . '_date'] = date('Y-m-d H:i:s', $article->date);
-            $csv[$key . '_comments'] = $article->comments->count;
-            $csv[$key . '_likes'] = $article->likes->count;
-            $csv[$key . '_thumbnail'] = $article->thumbnail_src;
+        foreach ($userData['media']['nodes'] as $key => $article) {
+            $csv[$key . '_date'] = date('Y-m-d H:i:s', $article['date']);
+            $csv[$key . '_comments'] = $article['comments']['count'];
+            $csv[$key . '_likes'] = $article['likes']['count'];
+            $csv[$key . '_thumbnail'] = $article['thumbnail_src'];
         }
         if ($isFirst) {
             fputcsv($fp, array_keys($csv));
@@ -290,5 +292,4 @@ function putReport($data)
         fputcsv($fp, $csv);
         $isFirst = false;
     }
-    fclose($fp);
 }
